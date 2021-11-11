cbbde9023743057837dd568b1a0de6ea2e73fe8d
DIALLO <mam.diallo163@gmail.com>
2021-11-11 21:44:38 +0100

    ajout controller Produit implem index et vue

---
+++ Gestion_Prod_Catalogue/Controllers/ProduitController.cs
@@ -0,0 +1,24 @@
+﻿using Gestion_Prod_Catalogue.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestion_Prod_Catalogue.Controllers
+{
+    public class ProduitController : Controller
+    {
+        private readonly CatalogueDbContext _db;
+
+        public ProduitController(CatalogueDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Produit> ListeProd = _db.Produits;
+            return View(ListeProd);
+        }
+    }
+}
